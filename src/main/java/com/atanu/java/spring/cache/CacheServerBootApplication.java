398511390e7687047a21bc439a2102560683bf00
Atanu Bhowmick <mail2atanu007@gmail.com>
2020-01-26 15:16:52 +0530

    Initial Commit

---
+++ src/main/java/com/atanu/java/spring/cache/CacheServerBootApplication.java
@@ -0,0 +1,24 @@
+package com.atanu.java.spring.offersvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author Atanu Bhowmick
+ *
+ */
+
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+public class CacheServerBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheServerBootApplication.class, args);
+	}
+
+}
