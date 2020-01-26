3d52f824350ba997f64d46b1952866fe3a28d6cf
Atanu Bhowmick <mail2atanu007@gmail.com>
2020-01-26 23:01:35 +0530

    Cache annotation added

--- src/main/java/com/atanu/java/spring/cache/CacheServerBootApplication.java
+++ src/main/java/com/atanu/java/spring/cache/CacheServerBootApplication.java
@@ -2,12 +2,14 @@ package com.atanu.java.spring.cache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author Atanu Bhowmick
  *
  */
 
+@EnableCaching
 @SpringBootApplication
 public class CacheServerBootApplication {
 
